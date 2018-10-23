06eec3955144c851ded3fd863835e60ef96d3ada
David Choi <daaave@gmail.com>
2018-10-23 08:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,10 @@ function destructivelyRemoveFirstKitten(){
   return kittens;
 }
 
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
